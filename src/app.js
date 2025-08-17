69c7646ef604634f179a48428159120c97a2a72b
Mehhul <mehulgargh@gmail.com>
2025-08-17 22:55:23 +0530

    delete and update apis added

--- src/app.js
+++ src/app.js
@@ -48,6 +48,28 @@ app.get("/feed", async (req, res) => {
     res.status(400).send("Something went wrong");
   }
 });
+// delete data
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// update data of user
+app.patch("/user", async (req,res)=>{
+  const data = req.body;
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndUpdate(userId,data);
+    res.send("User Updated successfully");
+  }catch(err){
+    res.status(404).send('Something went wrong');
+  }
+});
 
 connectDB()
   .then(() => {
